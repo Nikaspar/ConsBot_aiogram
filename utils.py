c65e922121c4cd3427b7098f6ce0c0f74837a94b
Nikaspar <sparrowv95@gmail.com>
2021-10-15 10:08:52 +0300

    update

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import sqlite3
-from datetime import datetime
 from config import *
 
 
@@ -10,13 +9,4 @@ def get_description(text):
         try:
             return cursor.execute("SELECT description FROM \'errors\' WHERE id = '%s'" % text).fetchall()[0][0]
         except IndexError:
-            return 'Ошибки с таким кодом нет в базе.'
-
-
-def get_photo(photo):
-    current_datetime = datetime.now()
-    with open("storage/{}.jpg".format(current_datetime.strftime("%d_%m_Y__%H_%M")), "wb") as file:
-        file.write(photo)
-
-def save_user(user):
-    pass
+            return 'Ошибки с таким кодом нет в базе.\nОтправьте фото ошибки в следующем сообщении.'
